35aa388b148cad2750b907784c5527968af08643
Brian Froschauer <brianfroschauer@Brians-MacBook-Pro.local>
2019-01-06 16:57:19 -0300

    Delete imports

--- src/main/java/resources/StoreResource.java
+++ src/main/java/resources/StoreResource.java
@@ -2,27 +2,16 @@ package resources;
 
 import dao.StoreDAO;
 import dao.exception.ConstraintViolationException;
-import dao.exception.DAOException;
 import filter.Secured;
 import model.Product;
 import model.Store;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-import util.FileManager;
-import util.Image;
 
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceException;
 import javax.validation.Valid;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.nio.file.Files;
-import java.util.Base64;
+import javax.ws.rs.core.*;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Author: brianfroschauer
@@ -31,12 +20,16 @@ import java.util.List;
 @Path("/stores")
 public class StoreResource {
 
+    @Context
+    private UriInfo uriInfo;
+
     public StoreResource() {}
 
     /**
      * Get store with de specified ID.
      *
      * @param storeId to get from database.
+     *
      * @return a store with the specified ID in the response.
      */
     @GET
@@ -44,15 +37,15 @@ public class StoreResource {
     @Path("/{storeId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getStore(@PathParam("storeId") Integer storeId) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            Store store = storeDao.get(Store.class, storeId).get();
+        final StoreDAO storeDao = new StoreDAO();
+        final Optional<Store> optionalStore = storeDao.get(Store.class, storeId);
+
+        if (optionalStore.isPresent()) {
+            final Store store = optionalStore.get();
             return Response.ok(store).build();
-        } catch (NoResultException e) {
-            throw new NotFoundException("Store, " + storeId + ", is not found");
-        } catch (RuntimeException e) {
-            throw new DAOException();
         }
+
+        throw new dao.exception.NotFoundException("Store, " + storeId + ", is not found");
     }
 
     /**
@@ -62,122 +55,83 @@ public class StoreResource {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getStores() {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            List<Store> stores = storeDao.getAll(Store.class);
-            return Response.ok(stores).build();
-        } catch (RuntimeException e) {
-            throw new DAOException();
-        }
+    public Response getAllStores() {
+        final StoreDAO storeDao = new StoreDAO();
+        final List<Store> stores = storeDao.getAll(Store.class);
+        return Response.ok(stores).build();
     }
 
     /**
      * Get a list of products from specified store filtered by category.
      *
      * @param storeId to get from database.
+     *
      * @return a list of the store's products in the response.
      */
     @GET
-    @Path("/{storeId}/products/{category}")
+    @Path("/{storeId}/products/{categoryId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProductsFromStore(@PathParam("storeId") Integer storeId,
-                                         @PathParam("category") String category) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            List<Product> products = storeDao.getProductsFromStore(storeId, 1); // ojo aca
-            return Response.ok(products).build();
-        } catch (RuntimeException e) {
-            throw new DAOException();
-        }
+    public Response getProductsFromStoreByCategory(@PathParam("storeId") Integer storeId,
+                                                   @PathParam("categoryId") Integer categoryId) {
+        final StoreDAO storeDao = new StoreDAO();
+        final List<Product> products = storeDao.getProductsFromStore(storeId, categoryId);
+        return Response.ok(products).build();
     }
 
     /**
      * Get a list of products from specified store.
      *
      * @param storeId to get from database.
+     *
      * @return a list of the store's products in the response.
      */
     @GET
     @Path("/{storeId}/products")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getProductsFromStore(@PathParam("storeId") Integer storeId) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            List<Product> products = storeDao.getProductsFromStore(storeId);
-            return Response.ok(products).build();
-        } catch (RuntimeException e) {
-            throw new DAOException();
-        }
+        final StoreDAO storeDao = new StoreDAO();
+        final List<Product> products = storeDao.getProductsFromStore(storeId);
+        return Response.ok(products).build();
     }
 
     /**
      * Search stores by key.
      *
      * @param key to be search.
+     *
      * @return a list with matched products.
      */
     @GET
-    @Path("/{storeId}/search/{key}")
+    @Path("/{storeId}/products/{key}")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response searchProductsFromStore(@PathParam("storeId") Integer storeId,
                                             @PathParam("key") String key) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            List<Product> products = storeDao.searchProductsFromStore(storeId, key);
-            return Response.ok(products).build();
-        } catch (RuntimeException e) {
-            throw new DAOException();
-        }
-    }
-
-    /**
-     * Get image from store.
-     *
-     * @param storeId to identify the image.
-     * @return the image in the response.
-     */
-    @GET
-    @Path("/image/{storeId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getStoreImage(@PathParam("storeId") Integer storeId) {
-        try {
-            File file = new File("/Users/brianfroschauer/Projects/nemesis/images/stores/" + storeId + ".jpg");
-            if (file.exists()) {
-                String encodedFile = Base64.getEncoder().withoutPadding().encodeToString(Files.readAllBytes(file.toPath()));
-                Image image = new Image(encodedFile);
-                return Response.ok(image).build();
-            } else {
-                File defaultFile = new File("/Users/brianfroschauer/Projects/nemesis/images/default/store.png");
-                String encodedFile = Base64.getEncoder().withoutPadding().encodeToString(Files.readAllBytes(defaultFile.toPath()));
-                Image image = new Image(encodedFile);
-                return Response.ok(image).build();
-            }
-        } catch (RuntimeException | IOException e) {
-            throw new DAOException(e.getMessage());
-        }
+        final StoreDAO storeDao = new StoreDAO();
+        final List<Product> products = storeDao.searchProductsFromStore(storeId, key);
+        return Response.ok(products).build();
     }
 
     /**
      * Add a new store in the database.
      *
      * @param store to be created.
-     * @return the created store in the response.
+     *
+     * @return the URI of the new resource in the response.
      */
     @POST
     @Secured
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addStore(@Valid Store store) {
-        StoreDAO storeDao = new StoreDAO();
+    public Response createStore(@Valid Store store) {
+        final StoreDAO storeDao = new StoreDAO();
         try {
-            Integer storeId = storeDao.create(store);
-            return Response.created(URI.create("/stores/" + storeId)).build();
-            // return Response.status(Response.Status.CREATED).entity(store).build();
+            final Integer storeId = storeDao.create(store);
+            final UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+            builder.path(storeId.toString());
+            return Response.created(builder.build()).build();
         } catch (PersistenceException e) {
             throw new ConstraintViolationException("Store, " + store.getName() + ", already exists");
-        } catch (RuntimeException e) {
-            throw new DAOException();
         }
     }
 
@@ -185,7 +139,8 @@ public class StoreResource {
      * Add product to store with the specified ID.
      *
      * @param storeId to get from database.
-     * @param product to be created.
+     * @param productId to be added to the store.
+     *
      * @return the created product in the response.
      */
     @POST
@@ -193,43 +148,17 @@ public class StoreResource {
     @Path("/{storeId}/products")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addProductToStore(@PathParam("storeId") Integer storeId, Product product) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            storeDao.addProductToStore(storeId, product.getId()); // ojo aca
-            return Response.ok(product).build();
-        } catch (RuntimeException e) {
-            throw new DAOException();
-        }
-    }
-
-    /**
-     * Upload store image.
-     *
-     * @param storeId to get from database.
-     * @return the updated user in the response.
-     */
-    @POST
-    @Secured
-    @Path("/images/{storeId}")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response uploadStoreImage(@PathParam("storeId") Integer storeId,
-                                    @FormDataParam("file") InputStream inputStream) {
-        try {
-            String fileName = "/" + storeId.toString() + ".jpg";
-            String filePath = "/stores" + fileName;
-            FileManager.writeImage(inputStream, filePath);
-            return Response.ok("Image uploaded successfully").build();
-        } catch (RuntimeException | IOException e) {
-            throw new DAOException();
-        }
+    public Response addProductToStore(@PathParam("storeId") Integer storeId, Integer productId) {
+        final StoreDAO storeDao = new StoreDAO();
+        storeDao.addProductToStore(storeId, productId);
+        return Response.ok().build();
     }
 
     /**
      * Update the specified store.
      *
      * @param store to be updated.
+     *
      * @return the updated store in the response.
      */
     @PUT
@@ -237,56 +166,31 @@ public class StoreResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateStore(Store store) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            storeDao.update(store);
-            return Response.ok(store).build();
-        } catch (RuntimeException e) {
-            throw new DAOException();
-        }
-    }
-
-    /**
-     * Delete the specified product from the store with the specified ID.
-     *
-     * @param storeId who has the store.
-     * @param productId to be deleted.
-     * @return a 204 HTTP status to confirm that the product has been deleted successfully.
-     */
-    @DELETE
-    @Secured
-    @Path("/{storeId}/products/{productId}")
-    public Response deleteProductFromStore(@PathParam("storeId") Integer storeId,
-                                           @PathParam("productId") Integer productId) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            //storeDao.deleteProductFromStore(storeId, productId);
-            return Response.noContent().build();
-        } catch (NullPointerException e) {
-            throw new NotFoundException("Store, " + storeId + " or Product, " + productId + ", is not found");
-        } catch (RuntimeException e) {
-            throw new DAOException();
-        }
+        final StoreDAO storeDao = new StoreDAO();
+        storeDao.update(store);
+        return Response.ok(store).build();
     }
 
     /**
      * Delete store with the specified ID.
      *
      * @param storeId to be deleted.
+     *
      * @return a 204 HTTP status to confirm that the store has been deleted successfully.
      */
     @DELETE
     @Secured
     @Path("/{storeId}")
     public Response deleteStore(@PathParam("storeId") Integer storeId) {
-        StoreDAO storeDao = new StoreDAO();
-        try {
-            //storeDao.delete(Store.class, storeId);
+        final StoreDAO storeDao = new StoreDAO();
+        final Optional<Store> optionalStore = storeDao.get(Store.class, storeId);
+
+        if (optionalStore.isPresent()) {
+            final Store store = optionalStore.get();
+            storeDao.delete(store);
             return Response.noContent().build();
-        } catch (IllegalArgumentException e) {
-            throw new NotFoundException("Store, " + storeId + ", is not found");
-        } catch (RuntimeException e) {
-            throw new DAOException();
         }
+
+        throw new NotFoundException("Store, " + storeId + ", is not found");
     }
 }
