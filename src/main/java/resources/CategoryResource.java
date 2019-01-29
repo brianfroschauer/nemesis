4dadfdbc9705a7488716b9218d8438ba32d21bdb
Brian Froschauer <brianfroschauer@Brians-MacBook-Pro.local>
2019-01-29 13:50:51 -0300

    Add category resource

---
+++ src/main/java/resources/CategoryResource.java
@@ -0,0 +1,83 @@
+package resources;
+
+import dao.CategoryDAO;
+import dao.exception.ConstraintViolationException;
+import filter.Secured;
+import model.Category;
+
+import javax.persistence.PersistenceException;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Author: brianfroschauer
+ * Date: 2019-01-29
+ */
+@Path("/categories")
+public class CategoryResource {
+
+    @Context
+    private UriInfo uriInfo;
+
+    /**
+     * Get category with de specified ID.
+     *
+     * @param categoryId to get from database.
+     *
+     * @return a store with the specified ID in the response.
+     */
+    @GET
+    @Path("/{categoryId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCategory(@PathParam("categoryId") Integer categoryId) {
+        final CategoryDAO categoryDAO = new CategoryDAO();
+        final Optional<Category> optionalCategory = categoryDAO.get(Category.class, categoryId);
+
+        if (optionalCategory.isPresent()) {
+            final Category category = optionalCategory.get();
+            return Response.ok(category).build();
+        }
+
+        throw new dao.exception.NotFoundException("Category, " + categoryId + ", is not found");
+    }
+
+    /**
+     * Get all existing categories.
+     *
+     * @return a list of existing categories in the response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllCategories() {
+        final CategoryDAO categoryDAO = new CategoryDAO();
+        final List<Category> categories = categoryDAO.getAll(Category.class);
+        return Response.ok(categories).build();
+    }
+
+    /**
+     * Add a new store in the database.
+     *
+     * @param category to be created.
+     *
+     * @return the URI of the new resource in the response.
+     */
+    @POST
+    @Secured
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createStore(@Valid Category category) {
+        final CategoryDAO categoryDAO = new CategoryDAO();
+        try {
+            final Integer categoryId = categoryDAO.create(category);
+            final UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+            builder.path(categoryId.toString());
+            return Response.created(builder.build()).build();
+        } catch (PersistenceException e) {
+            throw new ConstraintViolationException("Category, " + category.getCategory() + ", already exists");
+        }
+    }
+
+}
