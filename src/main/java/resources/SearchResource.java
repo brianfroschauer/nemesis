190a38b6910760edd424e1d2d050a871801f33f1
Brian Froschauer <brianfroschauer@Brians-MacBook-Pro.local>
2019-01-29 18:15:00 -0300

    Fix errors in search query

--- src/main/java/resources/SearchResource.java
+++ src/main/java/resources/SearchResource.java
@@ -6,10 +6,7 @@ import dao.exception.DAOException;
 import model.Product;
 import model.Store;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -57,5 +54,23 @@ public class SearchResource {
         } catch (RuntimeException e) {
             throw new DAOException();
         }
+    }
+
+    /**
+     * Search stores by key
+     *
+     * @param key to be search.
+     *
+     * @return a list with matched products.
+     */
+    @GET
+    @Path("/stores/{storeId}/products/{key}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response searchProductsFromStore(@PathParam("storeId") Integer storeId,
+                                            @PathParam("key") String key) {
+        final StoreDAO storeDao = new StoreDAO();
+        final List<Product> products = storeDao.searchProductsFromStore(storeId, key);
+        return Response.ok(products).build();
     }
 }
